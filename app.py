999aeb3b02ab02dd71273558885d5f03199ab429
Rayyan Ali Akbar <alphatrooper335@gmail.com>
2024-07-25 13:11:06 +0530

    updated code

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def predict_datapoint():
         print("after Prediction")
         return render_template('home.html',results=results[0])
     
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
