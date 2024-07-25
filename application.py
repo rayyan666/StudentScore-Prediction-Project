408543153a3f7691fe53588efa0f5266f9a2236a
Rayyan Ali Akbar <alphatrooper335@gmail.com>
2024-07-25 20:50:09 +0530

    updated

--- application.py
+++ application.py
@@ -7,13 +7,15 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 
+app=application
+
 ## Route for a home page
 
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('index.html') 
 
-@application.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -38,5 +40,7 @@ def predict_datapoint():
         print("after Prediction")
         return render_template('home.html',results=results[0])
     
-if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=8080, debug=True)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
+
